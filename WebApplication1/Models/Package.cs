d910df5c99dbadbffa677bc2a6dbd4f3591e3a0d
bogdan.marcu <bogdanmarcu2002@icloud.com>
2024-01-04 16:20:01 +0200

    Add project files.

---
+++ WebApplication1/Models/Package.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication1.Models
+{
+    public class Package
+    {
+        public int Id { get; set; }
+        public  User Sender { get; set; }
+        public  User Receiver { get; set; }
+        public int Weight { get; set; }
+
+    }
+}
